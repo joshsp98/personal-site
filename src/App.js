de2fb5df9a8bac5a111da07a0dab2bacf61af6db
Josh Spindler <joshuaspindler@outlook.com>
2020-04-16 22:00:45 +0100

    Fixed responsive design

--- src/App.js
+++ src/App.js
@@ -38,17 +38,17 @@ const LinkContainer = () => {
         {
             icon: faGithub,
             address: 'https://github.com/joshsp98',
-            key: 'gh-icon'
+            key: 'gh'
         },
         {
             icon: faLinkedin,
             address: 'https://linkedin.com/in/joshspindler',
-            key: 'li-icon'
+            key: 'li'
         },
         {
             icon: faEnvelope,
             address: 'mailto://hi@joshspindler.dev',
-            key: 'env-icon'
+            key: 'env'
         }
     ];
 
@@ -57,7 +57,9 @@ const LinkContainer = () => {
         <div id='icon-holder'>
             {
                 links.map(link => (
-                    <a className='icon-hyperlink' href={link.address} target='_blank'><FontAwesomeIcon icon={link.icon} size={'3x'}/></a>
+                    <a key={`${link.key}-icon`} href={link.address} className='icon-hyperlink' rel='noopener noreferrer' target='_blank'>
+                        <FontAwesomeIcon icon={link.icon} size={'3x'}/>
+                    </a>
                 ))
             }
         </div>
@@ -70,34 +72,8 @@ const App = () => {
             <NameContainer/>
             <RoleContainer/>
             <LinkContainer/>
-            {/*    make it all bullet points? put something about some projects? put something about portswigger?*/}
-            {/*links to github, linkedin, email?*/}
-            {/*need more stuff on here too*/}
-            {/*scroll down thing to go to projects - blinking arrow*/}
-            {/*header - 'about', 'some projects', links, on scroll*/}
-            {/*responsive*/}
-            {/*put on cv, linkedin, github*/}
-            {/*chsnge conshare docs to be the dommain/conshare*/}
-            {/*<div class="links">*/}
-            {/*    <a href="https://github.com/komaira"><i class="fa fa-github-square fa-3x" aria-hidden="true"></i></a>*/}
-            {/*    <a href="https://linkedin.com/in/ahmed-komaira"> <i class="fa fa-linkedin-square fa-3x" aria-hidden="true"*/}
-            {/*                                                        alt="linkedin"></i></a>*/}
-            {/*    <i class="fa fa-facebook-square fa-3x" aria-hidden="true"></i>*/}
-            {/*    <a href="https://twitter.com/akomaira"> <i class="fa fa-twitter-square fa-3x" aria-hidden="true"></i></a>*/}
-            {/*    <a href="/cdn-cgi/l/email-protection#67060c080a060e150627000a060e0b4904080a"><i class="fa fa-envelope fa-3x"*/}
-            {/*                                                                                    aria-hidden="true"></i></a>*/}
-            {/*</div>*/}
-            {/*<div class="links">*/}
-            {/*    <a href="https://github.com/komaira"><i class="fa fa-github-square fa-3x" aria-hidden="true"></i></a>*/}
-            {/*    <a href="https://linkedin.com/in/ahmed-komaira"> <i class="fa fa-linkedin-square fa-3x" aria-hidden="true"*/}
-            {/*                                                        alt="linkedin"></i></a>*/}
-            {/*    <i class="fa fa-facebook-square fa-3x" aria-hidden="true"></i>*/}
-            {/*    <a href="https://twitter.com/akomaira"> <i class="fa fa-twitter-square fa-3x" aria-hidden="true"></i></a>*/}
-            {/*    <a href="/cdn-cgi/l/email-protection#67060c080a060e150627000a060e0b4904080a"><i class="fa fa-envelope fa-3x"*/}
-            {/*                                                                                    aria-hidden="true"></i></a>*/}
-            {/*</div>*/}
         </div>
     )
-}
+};
 
 export default App;
