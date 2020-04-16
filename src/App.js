698fff5acd448176ccc8f639d1d776e07b7f4ea7
Josh Spindler <joshuaspindler@outlook.com>
2020-04-16 16:06:11 +0100

    Updated home and meta

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <p>Hello, world!</p>
+      <h2>Hi, I&apos;m</h2>
+        <h1>Josh Spindler</h1>
+        <h3>Software Engineer based in the UK</h3>
+        <a href={'https://github.com/joshsp98'}>GitHub</a>
     </div>
   );
 }
