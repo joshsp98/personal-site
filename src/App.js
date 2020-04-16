3ea17144f6548088dcf4e07a4ac4efcfc2ffae8e
Josh Spindler <joshuaspindler@outlook.com>
2020-04-16 16:16:50 +0100

    removed content

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h2>Hi, I&apos;m</h2>
-        <h1>Josh Spindler</h1>
-        <h3>Software Engineer based in the UK</h3>
-        <a href={'https://github.com/joshsp98'}>GitHub</a>
+      {/*<h2>Hi, I&apos;m</h2>*/}
+      {/*  <h1>Josh Spindler</h1>*/}
+      {/*  <h3>Software Engineer based in the UK</h3>*/}
+      {/*  <a href={'https://github.com/joshsp98'}>GitHub</a>*/}
     </div>
   );
 }
